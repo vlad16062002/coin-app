2f47a8b1ecd3e78e1db6229c65d18ffdf4a064d7
vlad16062002 <81857917+vlad16062002@users.noreply.github.com>
2021-04-03 23:05:24 +0300

    lesson 3

---
+++ configjg.js
@@ -0,0 +1,48 @@
+
+// Просто файл настройки бота, где можно указать основные параметры. Особо вдаваться в подробности, я не собираюсь
+
+
+module.exports = {
+
+  // Имя бота
+
+  botName:        `BapikBot`,
+
+  // Параметры группы
+
+    groupToken: 'b51c95126e9777cb1f8f4523abceb725c48b987c1f5c5e3128f0f85983a5708f98c7d4f87829cb4212098',
+    groupID: 199117984,
+
+  // b51c95126e9777cb1f8f4523abceb725c48b987c1f5c5e3128f0f85983a5708f98c7d4f87829cb4212098
+
+    vkcKey: `-f5nxPV6no7Ne0lfpUSBJ]ROY9e3ZlMpbabe;ataNgaK1*9uSN`,
+    vkcID: `625729206`,
+  
+  // PaperScroll
+    
+    psid: `625729206`,
+    pskey: `yOH5DLunF2f36PvJG04H0CnguAYQmhNJ0qioyubeE0mVjWlbcyv2s2RVDd13wapW`,
+
+  // CoronaCoin
+
+    cckey: `Y2rVsteG8Gwvpp65ce7XkmgyWWXk1KVe`,
+    ccid: `625729206`,
+
+  // GameCoin
+    gckey: `GtTxFsFqIU72odQtbVqEeAkj`,
+    gcid: `625729206`,
+
+
+  // База данных
+
+  urlDB:          'mongodb+srv://apmenlox1:apmenlox@cluster0.kjqmo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  nameDB:         'apmenlox1',
+
+  // Награды за топ
+
+    PSTop: [2000000, 1000000, 500000, 250000, 150000, 100000, 50000, 50000, 50000, 50000],
+
+
+
+
+};
